39158e4da90008b11ec8b7408664d0970abea904
Shate <saul.laplame@gmail.com>
2024-10-19 16:43:44 -0600

    se modifico la entrada  del servido en server.js

--- npaque-frontend/server.js
+++ npaque-frontend/server.js
@@ -1,6 +1,6 @@
 // server.js
 (async () => {
-    const serve = (await import('serve')).default;
-    const server = serve('build', { port: 3000 });
-    console.log('Serving on http://localhost:3000');
-  })();
+  const serve = (await import('serve')).default;
+  const server = serve('build', { port: 3000, host: '0.0.0.0' });
+  console.log('Serving on http://0.0.0.0:3000');
+})();
